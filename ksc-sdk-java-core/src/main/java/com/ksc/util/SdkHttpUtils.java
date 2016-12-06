c4a3ae0b8baaa400dbab6a301a150bcc3b0759ab
zhouguangrui <zhouguangrui@kingsoft.com>
2016-12-06 13:48:35 +0800

    fixed bug uri encode

--- ksc-sdk-java-core/src/main/java/com/ksc/util/SdkHttpUtils.java
+++ ksc-sdk-java-core/src/main/java/com/ksc/util/SdkHttpUtils.java
@@ -19,7 +19,6 @@ import java.net.URI;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -32,120 +31,186 @@ import com.ksc.http.HttpMethodName;
 public class SdkHttpUtils {
 
     private static final String DEFAULT_ENCODING = "UTF-8";
-    private static final Pattern ENCODED_CHARACTERS_PATTERN;
 
-    public SdkHttpUtils() {
+    /**
+     * Regex which matches any of the sequences that we need to fix up after
+     * URLEncoder.encode().
+     */
+    private static final Pattern ENCODED_CHARACTERS_PATTERN;
+    static {
+        StringBuilder pattern = new StringBuilder();
+
+        pattern
+            .append(Pattern.quote("+"))
+            .append("|")
+            .append(Pattern.quote("*"))
+            .append("|")
+            .append(Pattern.quote("%7E"))
+            .append("|")
+            .append(Pattern.quote("%2F"));
+
+        ENCODED_CHARACTERS_PATTERN = Pattern.compile(pattern.toString());
     }
 
-    public static String urlEncode(String value, boolean path) {
-        if(value == null) {
+    /**
+     * Encode a string for use in the path of a URL; uses URLEncoder.encode,
+     * (which encodes a string for use in the query portion of a URL), then
+     * applies some postfilters to fix things up per the RFC. Can optionally
+     * handle strings which are meant to encode a path (ie include '/'es
+     * which should NOT be escaped).
+     *
+     * @param value the value to encode
+     * @param path true if the value is intended to represent a path
+     * @return the encoded value
+     */
+    public static String urlEncode(final String value, final boolean path) {
+        if (value == null) {
             return "";
-        } else {
-            try {
-                String ex = URLEncoder.encode(value, "UTF-8");
-                Matcher matcher = ENCODED_CHARACTERS_PATTERN.matcher(ex);
+        }
 
-                StringBuffer buffer;
-                String replacement;
-                for(buffer = new StringBuffer(ex.length()); matcher.find(); matcher.appendReplacement(buffer, replacement)) {
-                    replacement = matcher.group(0);
-                    if("+".equals(replacement)) {
-                        replacement = "%20";
-                    } else if("*".equals(replacement)) {
-                        replacement = "%2A";
-                    } else if("%7E".equals(replacement)) {
-                        replacement = "~";
-                    } else if(path && "%2F".equals(replacement)) {
-                        replacement = "/";
-                    }
+        try {
+            String encoded = URLEncoder.encode(value, DEFAULT_ENCODING);
+
+            Matcher matcher = ENCODED_CHARACTERS_PATTERN.matcher(encoded);
+            StringBuffer buffer = new StringBuffer(encoded.length());
+
+            while (matcher.find()) {
+                String replacement = matcher.group(0);
+
+                if ("+".equals(replacement)) {
+                    replacement = "%20";
+                } else if ("*".equals(replacement)) {
+                    replacement = "%2A";
+                } else if ("%7E".equals(replacement)) {
+                    replacement = "~";
+                } else if (path && "%2F".equals(replacement)) {
+                    replacement = "/";
                 }
 
-                matcher.appendTail(buffer);
-                return buffer.toString();
-            } catch (UnsupportedEncodingException var6) {
-                throw new RuntimeException(var6);
+                matcher.appendReplacement(buffer, replacement);
             }
+
+            matcher.appendTail(buffer);
+            return buffer.toString();
+
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
-    public static String urlDecode(String value) {
-        if(value == null) {
+    /**
+     * Decode a string for use in the path of a URL; uses URLDecoder.decode,
+     * which decodes a string for use in the query portion of a URL.
+     *
+     * @param value The value to decode
+     * @return The decoded value if parameter is not null, otherwise, null is returned.
+     */
+    public static String urlDecode(final String value) {
+        if (value == null) {
             return null;
-        } else {
-            try {
-                return URLDecoder.decode(value, "UTF-8");
-            } catch (UnsupportedEncodingException var2) {
-                throw new RuntimeException(var2);
-            }
+        }
+
+        try {
+            return URLDecoder.decode(value, DEFAULT_ENCODING);
+
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
+    /**
+     * Returns true if the specified URI is using a non-standard port (i.e. any
+     * port other than 80 for HTTP URIs or any port other than 443 for HTTPS
+     * URIs).
+     *
+     * @param uri
+     *
+     * @return True if the specified URI is using a non-standard port, otherwise
+     *         false.
+     */
     public static boolean isUsingNonDefaultPort(URI uri) {
-        String scheme = uri.getScheme().toLowerCase();
+        String scheme = StringUtils.lowerCase(uri.getScheme());
         int port = uri.getPort();
-        return port <= 0?false:(scheme.equals("http") && port == 80?false:!scheme.equals("https") || port != 443);
+
+        if (port <= 0) return false;
+        if (scheme.equals("http") && port == 80) return false;
+        if (scheme.equals("https") && port == 443) return false;
+
+        return true;
     }
 
     public static boolean usePayloadForQueryParameters(SignableRequest<?> request) {
         boolean requestIsPOST = HttpMethodName.POST.equals(request.getHttpMethod());
-        boolean requestHasNoPayload = request.getContent() == null;
+        boolean requestHasNoPayload = (request.getContent() == null);
+
         return requestIsPOST && requestHasNoPayload;
     }
 
+    /**
+     * Creates an encoded query string from all the parameters in the specified
+     * request.
+     *
+     * @param request
+     *            The request containing the parameters to encode.
+     *
+     * @return Null if no parameters were present, otherwise the encoded query
+     *         string for the parameters present in the specified request.
+     */
     public static String encodeParameters(SignableRequest<?> request) {
-        Map requestParams = request.getParameters();
-        if(requestParams.isEmpty()) {
-            return null;
-        } else {
-            ArrayList nameValuePairs = new ArrayList();
-            Iterator var3 = requestParams.entrySet().iterator();
 
-            while(var3.hasNext()) {
-                Entry entry = (Entry)var3.next();
-                String parameterName = (String)entry.getKey();
-                Iterator var6 = ((List)entry.getValue()).iterator();
+        final Map<String, List<String>> requestParams = request.getParameters();
 
-                while(var6.hasNext()) {
-                    String value = (String)var6.next();
-                    nameValuePairs.add(new BasicNameValuePair(parameterName, value));
-                }
+        if (requestParams.isEmpty()) return null;
+
+        final List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+        for (Entry<String, List<String>> entry : requestParams.entrySet()) {
+            String parameterName = entry.getKey();
+            for (String value : entry.getValue()) {
+                nameValuePairs
+                    .add(new BasicNameValuePair(parameterName, value));
             }
-
-            return URLEncodedUtils.format(nameValuePairs, "UTF-8");
         }
+
+        return URLEncodedUtils.format(nameValuePairs, DEFAULT_ENCODING);
     }
 
+    /**
+     * Append the given path to the given baseUri.
+     * By default, all slash characters in path will not be url-encoded.
+     */
     public static String appendUri(String baseUri, String path) {
         return appendUri(baseUri, path, false);
     }
 
-    public static String appendUri(String baseUri, String path, boolean escapeDoubleSlash) {
+    /**
+     * Append the given path to the given baseUri.
+     *
+     * @param baseUri The URI to append to (required, may be relative)
+     * @param path The path to append (may be null or empty).  Path should be pre-encoded.
+     * @param escapeDoubleSlash Whether double-slash in the path should be escaped to "/%2F"
+     * @return The baseUri with the path appended
+     */
+    public static String appendUri(final String baseUri, String path, final boolean escapeDoubleSlash) {
         String resultUri = baseUri;
-        if(path != null && path.length() > 0) {
-            if(path.startsWith("/")) {
-                if(baseUri.endsWith("/")) {
-                    resultUri = baseUri.substring(0, baseUri.length() - 1);
+        if (path != null && path.length() > 0) {
+            if (path.startsWith("/")) {
+                // trim the trailing slash in baseUri, since the path already starts with a slash
+                if (resultUri.endsWith("/")) {
+                    resultUri = resultUri.substring(0, resultUri.length() - 1);
                 }
-            } else if(!baseUri.endsWith("/")) {
-                resultUri = baseUri + "/";
+            } else if (!resultUri.endsWith("/")) {
+                resultUri += "/";
             }
-
-            String encodedPath = urlEncode(path, true);
-            if(escapeDoubleSlash) {
-                encodedPath = encodedPath.replace("//", "/%2F");
+            if (escapeDoubleSlash) {
+                resultUri += path.replace("//", "/%2F");
+            } else {
+                resultUri += path;
             }
-
-            resultUri = resultUri + encodedPath;
-        } else if(!baseUri.endsWith("/")) {
-            resultUri = baseUri + "/";
+        } else if (!resultUri.endsWith("/")) {
+            resultUri += "/";
         }
 
         return resultUri;
-    }
-
-    static {
-        StringBuilder pattern = new StringBuilder();
-        pattern.append(Pattern.quote("+")).append("|").append(Pattern.quote("*")).append("|").append(Pattern.quote("%7E")).append("|").append(Pattern.quote("%2F"));
-        ENCODED_CHARACTERS_PATTERN = Pattern.compile(pattern.toString());
     }
 }
