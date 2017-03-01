07de12b88e38b1852df7d12437356f989df3f8ce
齐超 <qichao@kingsoft.com>
2017-03-01 17:20:20 +0800

    add comments to TopReferRequest

--- ksc-sdk-java-cdn/src/main/java/com/ksc/cdn/model/statistic/top/refer/TopReferRequest.java
+++ ksc-sdk-java-cdn/src/main/java/com/ksc/cdn/model/statistic/top/refer/TopReferRequest.java
@@ -10,7 +10,10 @@ import java.text.ParseException;
 import java.util.Map;
 
 /**
- * Created by qichao on 2017/2/27.
+ * 获取域名某天内某一时段的热门页面访问数据排名，仅包含Top200且访问数大于15次的热门页面的访问次数、访问流量，并按次数排名
+ * 支持批量域名查询，多个域名ID用逗号（半角）分隔
+ * 最多可获取最近一年内24小时跨度的数据
+ * 时效性：30分钟延迟
  */
 public class TopReferRequest extends CommonFieldRequest {
 
@@ -30,15 +33,15 @@ public class TopReferRequest extends CommonFieldRequest {
     @Override
     public Map<String, String> buildParams() throws KscClientException, ParseException {
         CommonValidUtil.check(this);
-        Map<String,String> params= super.buildParams();
-        if(StringUtils.isNotBlank(this.getLimitN()))
-            params.put("LimitN",this.getLimitN());
+        Map<String, String> params = super.buildParams();
+        if (StringUtils.isNotBlank(this.getLimitN()))
+            params.put("LimitN", this.getLimitN());
         return params;
     }
 
     @Override
     public GeneralRequestParam getGeneralRequestParam() {
-        return new GeneralRequestParam("GetTopReferData","2016-09-01","/2016-09-01/statistics/GetTopReferData");
+        return new GeneralRequestParam("GetTopReferData", "2016-09-01", "/2016-09-01/statistics/GetTopReferData");
     }
 
 }
