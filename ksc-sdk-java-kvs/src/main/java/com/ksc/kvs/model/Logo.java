b78cf1578824e599ef1865a1692b1e0c40c7c4b2
w_gaoyuan1 [高远] <w_gaoyuan1@kingsoft.cn>
2017-05-23 16:40:59 +0800

    update

---
+++ ksc-sdk-java-kvs/src/main/java/com/ksc/kvs/model/Logo.java
@@ -0,0 +1,95 @@
+package com.ksc.kvs.model;
+
+public class Logo {
+	private String url;
+	private String logotype;
+	private String usage_scenario;
+	private String effect_objects;
+	private int shortSide;
+	private int disable_scale;
+	private int offsetX;
+	private int offsetY;
+	private int ss;
+	private int duration;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getLogotype() {
+		return logotype;
+	}
+
+	public void setLogotype(String logotype) {
+		this.logotype = logotype;
+	}
+
+	public String getUsage_scenario() {
+		return usage_scenario;
+	}
+
+	public void setUsage_scenario(String usage_scenario) {
+		this.usage_scenario = usage_scenario;
+	}
+
+	public String getEffect_objects() {
+		return effect_objects;
+	}
+
+	public void setEffect_objects(String effect_objects) {
+		this.effect_objects = effect_objects;
+	}
+
+	public int getShortSide() {
+		return shortSide;
+	}
+
+	public void setShortSide(int shortSide) {
+		this.shortSide = shortSide;
+	}
+
+	public int getDisable_scale() {
+		return disable_scale;
+	}
+
+	public void setDisable_scale(int disable_scale) {
+		this.disable_scale = disable_scale;
+	}
+
+	public int getOffsetX() {
+		return offsetX;
+	}
+
+	public void setOffsetX(int offsetX) {
+		this.offsetX = offsetX;
+	}
+
+	public int getOffsetY() {
+		return offsetY;
+	}
+
+	public void setOffsetY(int offsetY) {
+		this.offsetY = offsetY;
+	}
+
+	public int getSs() {
+		return ss;
+	}
+
+	public void setSs(int ss) {
+		this.ss = ss;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+}
